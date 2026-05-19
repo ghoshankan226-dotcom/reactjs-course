20a51a1c992f669eded53be01733c4ab0ffdaf0c
Ankan Ghosh <ankanghosh23102002@gmail.com>
2026-05-19 13:57:49 +0530

    react1 done

---
+++ react1/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900">
+      <h1 className="text-5xl font-bold text-white">My App</h1>
+    </div>
+  )
+}
+
+export default App
